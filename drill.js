65c0e43434192bfcf9728669583209c3f79d62ab
seankrummel <sean.krummel@gmail.com>
2018-10-29 14:17:34 -0500

    added decode function

--- drill.js
+++ drill.js
@@ -16,8 +16,41 @@ function beyond(num) {
     }
 }
 
+function decode(word) {
+    let num;
+    switch (word[0]) {
+        case 'a':
+            num = 2;
+            break;
+        case 'b':
+            num = 3;
+            break;
+        case 'c':
+            num = 4;
+            break;
+        case 'd':
+            num = 5;
+            break;
+        default:
+            num = 0;
+            break;
+    }
+    if (!num) {
+        return ' ';
+    }
+    return word[num - 1];
+}
+
 console.log(jediName('Sean', 'Krummel'));
 beyond(undefined);
 beyond(1);
 beyond(-1);
 beyond(0);
+console.log(decode('craft'));
+console.log(decode('block'));
+console.log(decode('argon'));
+console.log(decode('meter'));
+console.log(decode('bells'));
+console.log(decode('brown'));
+console.log(decode('croon'));
+console.log(decode('droop'));
